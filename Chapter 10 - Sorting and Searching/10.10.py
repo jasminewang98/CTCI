55d60bce10f3b4da12bd195346e8d9740bd6b6ca
Jasmine Wang <jasminewang76@gmail.com>
2017-08-11 12:04:37 -0400

    questions for chapter 10

---
+++ Chapter 10 - Sorting and Searching/10.10.py
@@ -0,0 +1,5 @@
+# 10.1 o Rank from Stream: Imagine you are reading in a stream of integers. Periodically, you wish to be able
+# to look up the rank of a numberx (the number of values less than or equal to x). lmplementthe data
+# structures and algorithms to support these operations. That is, implement the method track ( int
+# x), which is called when each number is generated, and the method getRankOfNumber(int
+# x), which returns the number of values less than or equal to x (not including x itself). 
---
+++ Chapter 10 - Sorting and Searching/10.5.py
@@ -0,0 +1,2 @@
+# 10.5 Sparse Search: Given a sorted array of strings that is interspersed with empty strings, write a
+# method to find the location of a given string. 
