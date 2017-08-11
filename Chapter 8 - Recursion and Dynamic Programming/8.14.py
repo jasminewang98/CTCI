6174afb55ac728afc65b3622cb9ed09604d67418
Jasmine Wang <jasminewang76@gmail.com>
2017-08-11 12:00:36 -0400

    questions for chapter 8

---
+++ Chapter 8 - Recursion and Dynamic Programming/8.14.py
@@ -0,0 +1,3 @@
+# 8.14 Boolean Evaluation: Given a boolean expression consisting of the symbols 0 (false), 1 (true), &
+# (AND), I (OR), and /\ (XOR), and a desired boolean result value result, implement a function to
+# count the number of ways of parenthesizing the expression such that it evaluates to result. 
---
+++ Chapter 8 - Recursion and Dynamic Programming/8.4.py
@@ -0,0 +1,1 @@
+# 8.4 Power Set: Write a method to return all subsets of a set. 
